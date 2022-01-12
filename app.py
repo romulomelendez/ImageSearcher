336c54bb148e00446bad0a936d631754cdda8678
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-01-12 17:14:32 -0300

    web scrapping working

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from webscrapping import getImage
 
 app = Flask(__name__)
 
 #routes
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def home():
+    value = request.form['stuff']
+    getImage(value)
     return render_template('index.html')
 
 if __name__ == '__main__':
